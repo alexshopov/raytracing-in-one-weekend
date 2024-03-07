0633d88580d3b32205b67926ed38ac6e8cf2abd4
Alex Shopov <shopov.alex@gmail.com>
2024-03-07 09:29:04 -0500

    write output to file, default filename 'out.ppm'

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#include <fstream>
 
 void render(int width, int height) {
-    std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+    std::ofstream out;
+    out.open("out.ppm");
+
+    out << "P3\n" << width << ' ' << height << "\n255\n";
 
     for (int j = 0; j < height; ++j) {
         std::clog << "\nScanlines remaining: " << (height - j) << ' ' << std::flush;
         for (int i = 0; i < width; ++i) {
             auto r = double(i) / (width - 1);
             auto g = double(j) / (height - 1);
-            auto b = 1;
+            auto b = 0.5;
 
             int ir = static_cast<int>(255.999 * r);
             int ig = static_cast<int>(255.999 * g);
             int ib = static_cast<int>(255.999 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            out << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
 
+    out.close();
     std::clog << "\nDone.          \n";
 }
 

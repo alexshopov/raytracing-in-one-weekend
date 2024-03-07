b95a13619bfd9198cd232d27f14b3aa1edd82bc7
Alex Shopov <shopov.alex@gmail.com>
2024-03-07 09:26:14 -0500

    write output to stdout

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
+void render(int width, int height) {
+    std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+
+    for (int j = 0; j < height; ++j) {
+        std::clog << "\nScanlines remaining: " << (height - j) << ' ' << std::flush;
+        for (int i = 0; i < width; ++i) {
+            auto r = double(i) / (width - 1);
+            auto g = double(j) / (height - 1);
+            auto b = 1;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    std::clog << "\nDone.          \n";
+}
+
 int main(int, char**){
-    std::cout << "Hello, from raytracing-in-one-weekend!\n";
+    // image properties
+    int image_width = 256;
+    int image_height = 256;
+
+    render(image_width, image_height);
 }

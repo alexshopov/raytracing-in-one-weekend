43bdbbe2ff464815f38d191d4b178813feb4c08b
Alex Shopov <shopov.alex@gmail.com>
2024-03-08 13:21:43 -0500

    added fuzziness to reflections

--- src/material.h
+++ src/material.h
@@ -37,15 +37,16 @@ public:
 
 class metal : public material {
     color albedo;
+    double fuzz;
 
 public:
-    metal(const color &a) : albedo{a} {}
+    metal(const color &a, double f) : albedo{a}, fuzz{f} {}
 
     bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.n);
-        scattered = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected + fuzz * random_unit_vector());
         attenuation = albedo;
-        return true;
+        return scattered.direction().dot(rec.n) > 0;
     }
 };
 

e13da905d8eb2440cb27d06700e76ce749eec8a5
Alex Shopov <shopov.alex@gmail.com>
2024-03-07 19:24:44 -0500

    added ray class, camera, and viewport

--- main.cpp
+++ main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
 #include <fstream>
 
-#include "color.h"
-#include "vec3.h"
+#include "src/color.h"
+#include "src/ray.h"
+#include "src/vec3.h"
 
-void render(int width, int height) {
+color ray_color(const ray &r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y + 1.);
+    return (1. - a) * color{1., 1., 1.} + a * color{0.5, 0.7, 1.};
 }
 
 int main(int, char**){
     // image properties
-    int image_width = 256;
-    int image_height = 256;
+    //--------------------------------------------------------------
+    auto aspect_ratio = 16./9.;
+    int image_width = 400;
 
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // camera properties
+    //--------------------------------------------------------------
+    auto focal_length = 1.;
+    auto viewport_height = 2.;
+    auto viewport_width = viewport_height
+        * (static_cast<double>(image_width) / image_height);
+    auto camera_center = point3{0, 0, 0};
+
+    auto viewport_u = vec3{viewport_width, 0, 0};
+    auto viewport_v = vec3{0, -viewport_height, 0};
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center
+        - vec3{0, 0, focal_length} - viewport_u / 2 - viewport_v / 2;
+    auto pixel00_loc = viewport_upper_left + 0.5
+        * (pixel_delta_u + pixel_delta_v);
+
+    // open output file
+    //--------------------------------------------------------------
     std::ofstream out;
     out.open("out.ppm");
     if (!out.is_open()) {
@@ -19,20 +48,27 @@ int main(int, char**){
         exit(1);
     }
 
+    // render
+    //--------------------------------------------------------------
     out << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; ++j) {
         std::clog << "\nScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 
         for (int i = 0; i < image_width; ++i) {
-            auto pixel_color = color(double(i) / (image_width - 1),
-                                     double(j) / (image_height - 1),
-                                     0);
+            auto pixel_center =
+                pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r{camera_center, ray_direction};
+
+            color pixel_color = ray_color(r);
             write_color(out, pixel_color);
         }
     }
 
     std::clog << "\nDone.          \n";
 
+    // clean up
+    //--------------------------------------------------------------
     out.close();
 }

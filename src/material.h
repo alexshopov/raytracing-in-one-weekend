1d22b0629d6a7ec61dbeddd9f4ada3a3e11fb371
Alex Shopov <shopov.alex@gmail.com>
2024-03-08 13:32:50 -0500

    added dielectric refraction

--- src/material.h
+++ src/material.h
@@ -50,4 +50,22 @@ public:
     }
 };
 
+class dielectric : public material {
+    double ir; // index of refraction
+
+public:
+    dielectric(double index_of_refraction) : ir{index_of_refraction} {}
+
+    bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override {
+        attenuation = color{1., 1., 1.};
+        double refraction_ratio = rec.front_face ? (1. / ir) : ir;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        vec3 refracted = refract(unit_direction, rec.n, refraction_ratio);
+
+        scattered = ray(rec.p, refracted);
+        return true;
+    }
+};
+
 #endif

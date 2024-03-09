721203500c35caaec20b8b81ff4c0cabf74f19a6
Alex Shopov <shopov.alex@gmail.com>
2024-03-08 23:00:41 -0500

    defocus blur and final scene

--- src/camera.h
+++ src/camera.h
@@ -16,6 +16,8 @@ class camera {
     vec3 pixel_delta_u;
     vec3 pixel_delta_v;
     vec3 u, v, w;
+    vec3 defocus_disk_u;
+    vec3 defocus_disk_v;
 
     void initialize() {
         image_height = static_cast<int>(image_width / aspect_ratio);
@@ -23,10 +25,9 @@ class camera {
 
         center = lookfrom;
 
-        auto focal_length = (lookfrom - lookat).length();
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
-        auto viewport_height = 2. * h * focal_length;
+        auto viewport_height = 2. * h * focus_dist;
         auto viewport_width = viewport_height
             * (static_cast<double>(image_width) / image_height);
 
@@ -40,8 +41,13 @@ class camera {
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
-        auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
+        auto viewport_upper_left = center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        // camera defocus disk basis vectors
+        auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2.));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
     }
 
     color ray_color(const ray &r, int depth, const hittable &world) const {
@@ -63,11 +69,19 @@ class camera {
         return (1. - a) * color{1., 1., 1.} + a * color{0.5, 0.7, 1.};
     }
 
+    /**
+     * Returns a random point in the camera defocus disk
+     */
+    point3 defocus_disk_sample() const {
+        auto p = random_in_unit_disk();
+        return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+    }
+
     ray get_ray(int i, int j) const {
         auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
         auto pixel_sample = pixel_center + pixel_sample_square();
 
-        auto ray_origin = center;
+        auto ray_origin = defocus_angle <= 0 ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
         return ray(ray_origin, ray_direction);
@@ -81,14 +95,17 @@ class camera {
 
 public:
     double aspect_ratio = 1.;
-    int image_width = 100;
-    int samples_per_pixel = 10;
-    int max_depth = 10;
+    int image_width = 100;      // rendered image width
+    int samples_per_pixel = 10; // count of random samples for each pixel
+    int max_depth = 10;         // max numnber of ray bounces into scene
 
-    double vfov     = 90.; // vertical view angle
-    point3 lookfrom = point3{0, 0, -1};
-    point3 lookat   = point3{0, 0, 0};
-    vec3   vup      = vec3{0, 1, 0};
+    double vfov     = 90.;              // vertical view angle
+    point3 lookfrom = point3{0, 0, -1}; // point camera is looking fomr
+    point3 lookat   = point3{0, 0, 0};  // point camera is looking at
+    vec3   vup      = vec3{0, 1, 0};    // camera up vector
+
+    double defocus_angle = 0; // variation angle of rays through each pixel
+    double focus_dist = 10;   // distance from camera lookfrom point to focul plane
 
     void render(const hittable &world) {
         initialize();
